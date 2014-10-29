b773033027ebe928b7652b46c17657313bf96c59
Zhen Chen <chenzhen42@qq.com>
2014-10-29 15:24:17 +1300

    Support sending news messages
    Now it can deal with the event from menu or text from user by responsing
    text content or articles with pictures (news type) according to the data
    in the database.

--- index.php
+++ index.php
@@ -1,15 +1,16 @@
 <?php
 //define your token
 define("TOKEN", "weixin");
-$wechatObj = new wechatCallbackapi();
+$wechatObj = new Wechat();
 if (isset($_GET["echostr"]))
     $wechatObj -> valid();
 else
     $wechatObj -> responseMsg();
 
-class wechatCallbackapi
+class Wechat
 {
     private $mydb;
+    private $postObj;
 
     public function __construct()
     {
@@ -40,74 +41,102 @@ class wechatCallbackapi
             /* libxml_disable_entity_loader is to prevent XML eXternal Entity Injection,
                 the best way is to check the validity of xml by yourself */
             libxml_disable_entity_loader(true);
-            $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
-            $msgType = trim($postObj -> MsgType);
-            $resultStr = "";
+            $this -> postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+            $msgType = trim($this -> postObj -> MsgType);
 
             switch ($msgType)
             {
                 case "text":
-                    $resultStr = $this -> handleText($postObj);
+                    $resultStr = $this -> handleText();
                     break;
                 case "event":
-                    $resultStr = $this -> handleEvent($postObj);
+                    $resultStr = $this -> handleEvent();
                     break;
             }
-            //$resultStr is the final output
-            echo $resultStr;
-            exit();
         }
-        else
-        {
-            echo "";
-            exit();
-        }
+
+        //$resultStr is the final output
+        echo $resultStr;
+        exit();
     }
 
-    public function handleText($postObj)
+    private function handleText()
     {
-        //$contentStr is the message we want to send back
-        $contentStr = "";
-
         //$keyword is the message from the user
-        $keyword = trim($postObj -> Content);
+        //$this -> postObj -> Content is created by user, so trim() method needs to be applied
+        $keyword = trim($this -> postObj -> Content);
 
-        $sql = "SELECT reply FROM keyword WHERE keyword = '%s'";
+        $sql = "SELECT * FROM reply WHERE keyword = '%s' ORDER BY no";
         $sql = sprintf($sql, $keyword);
-        $result = $this -> mydb -> query($sql);
-        if ($row = $result -> fetch_assoc())
-            $contentStr = $row["reply"];
 
-        $resultStr = $this -> responseText($postObj, $contentStr);
-        return $resultStr;
+        return $this -> response($sql);
     }
 
-    public function handleEvent($postObj)
+    private function handleEvent()
     {
-        //$contentStr is the message we want to send back
-        $contentStr = "";
-
         //get the type and the key of this event
-        $eventType = $postObj -> Event;
-        $eventKey = $postObj -> EventKey;
+        $event = $this -> postObj -> Event;
+        $keyword = $this -> postObj -> EventKey;
 
-        $sql = "SELECT reply FROM event WHERE event = '%s' and eventKey = '%s'";
-        $sql = sprintf($sql, $eventType, $eventKey);
-        $result = $this -> mydb -> query($sql);
-        if ($row = $result -> fetch_assoc())
-            $contentStr = $row["reply"];
+        $sql = "SELECT * FROM reply WHERE event = '%s' and keyword = '%s' ORDER BY no";
+        $sql = sprintf($sql, $event, $keyword);
 
-        $resultStr = $this -> responseText($postObj, $contentStr);
+        return $this -> response($sql);
+    }
+
+    private function response($sql)
+    {
+        if ($result = $this -> mydb -> query($sql))
+        {
+            $row = $result -> fetch_assoc();
+            $msgType = $row["msgType"];
+            switch ($msgType)
+            {
+                case "text":
+                    //$contentStr is the text message we want to send back
+                    $contentStr = $row["content"];
+                    $resultStr = $this -> responseText($contentStr);
+                    break;
+                case "news":
+                    $itemTpl = "<item>
+                                <Title><![CDATA[%s]]></Title>
+                                <Description><![CDATA[%s]]></Description>
+                                <PicUrl><![CDATA[%s]]></PicUrl>
+                                <Url><![CDATA[%s]]></Url>
+                                </item>";
+                    $no = 0;
+                    do
+                    {
+                        $title = $row["title"];
+                        $description = $row["description"];
+                        $picurl = $row["picurl"];
+                        $url = $row["url"];
+                        if (strlen($title.$description.$picurl.$url) > 0)
+                        {
+                            //$itemStr consists of all the information of an article
+                            $itemStr = sprintf($itemTpl, $title, $description, $picurl, $url);
+                            //$articleStr consists of all the articles
+                            $articleStr .= $itemStr;
+                            $no++;
+                        }
+                    } while ($row = $result -> fetch_assoc());
+                    $resultStr = $this -> responseNews($articleStr, $no);
+                    break;
+            }
+            $result -> free();
+            $this -> mydb -> close();
+        }
+
         return $resultStr;
     }
 
-    public function responseText($postObj, $contentStr)
+    private function responseText($contentStr)
     {
         if (strlen($contentStr) == 0)
             return "";
 
-        $fromUsername = $postObj -> FromUserName;
-        $toUsername = $postObj -> ToUserName;
+        $fromUsername = $this -> postObj -> ToUserName;
+        $toUsername = $this -> postObj -> FromUserName;
         $time = time();
         $msgType = "text";
         $textTpl = "<xml>
@@ -117,7 +146,28 @@ class wechatCallbackapi
                     <MsgType><![CDATA[%s]]></MsgType>
                     <Content><![CDATA[%s]]></Content>
                     </xml>";
-        $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+        $resultStr = sprintf($textTpl, $toUsername, $fromUsername, $time, $msgType, $contentStr);
+        return $resultStr;
+    }
+
+    private function responseNews($articleStr, $no)
+    {
+        if ($no == 0)
+            return "";
+
+        $fromUsername = $this -> postObj -> ToUserName;
+        $toUsername = $this -> postObj -> FromUserName;
+        $time = time();
+        $msgType = "news";
+        $newsTpl = "<xml>
+                    <ToUserName><![CDATA[%s]]></ToUserName>
+                    <FromUserName><![CDATA[%s]]></FromUserName>
+                    <CreateTime>%s</CreateTime>
+                    <MsgType><![CDATA[%s]]></MsgType>
+                    <ArticleCount>%d</ArticleCount>
+                    <Articles>%s</Articles>
+                    </xml>";
+        $resultStr = sprintf($newsTpl, $toUsername, $fromUsername, $time, $msgType, $no, $articleStr);
         return $resultStr;
     }
 
